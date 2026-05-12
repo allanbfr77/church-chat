7e255f1c9133ca45031d329daad8b520b417d876
allanbfr77 <alan.tcn1@gmail.com>
2026-05-12 18:06:14 -0300

    fix: firebase.js syntax

--- church-chat/src/firebase.js
+++ church-chat/src/firebase.js
@@ -1,14 +1,14 @@
-git add .import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyBFEBZIYHApja2wkWqkpVDNEdM7-93IlWs",
-  authDomain:        "church-chat-dd8ea.firebaseapp.com",
-  databaseURL:       "https://church-chat-dd8ea-default-rtdb.firebaseio.com",
-  projectId:         "church-chat-dd8ea",
-  storageBucket:     "church-chat-dd8ea.appspot.com",
+  apiKey: "AIzaSyBFEBZIYHApja2wkWqkpVDNEdM7-93IlWs",
+  authDomain: "church-chat-dd8ea.firebaseapp.com",
+  databaseURL: "https://church-chat-dd8ea-default-rtdb.firebaseio.com",
+  projectId: "church-chat-dd8ea",
+  storageBucket: "church-chat-dd8ea.appspot.com",
   messagingSenderId: "695515727180",
-  appId:             "1:695515727180:web:dca5e78b722a7e3649bbe9",
+  appId: "1:695515727180:web:dca5e78b722a7e3649bbe9"
 }
 
 const app = initializeApp(firebaseConfig)
